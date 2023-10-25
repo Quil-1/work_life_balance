4588ede37879b6a4989513264f167a44ea966bc2
Adelaja Qowiyyu <adexolamilekan46@gmail.com>
2023-10-25 16:51:26 +0100

    This PR contains the basic feature of the app

---
+++ settings.py
@@ -0,0 +1,12 @@
+"""
+This contains the constants needed for the shutdown utility
+
+"""
+
+LINUX_OS = "linux"
+WINDOWS_OS = "win32"
+MAC_OS = "darwin"
+
+WINDOWS_OS_SHUTDOWN_COMMAND = "shutdown /s /t"
+
+WINDOWS_OS_LOGOUT_COMMAND = "shutdown -l"
